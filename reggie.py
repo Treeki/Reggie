e0e005282a2519c9dfee5279c2abeae51bfe172d
Omega <omega@omega-desktop.(none)>
2010-04-01 23:13:22 -0400

    Applied Angel-SL's path editing patch

--- reggie.py
+++ reggie.py
@@ -18,6 +18,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+
 import archive
 import lz77
 import os.path
